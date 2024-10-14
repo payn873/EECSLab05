ad21be0ae37ee81342a16ae54510c2ded6e7e022
payn873 <142923202+payn873@users.noreply.github.com>
2024-10-13 19:37:29 -0500

    Update code1.c
    Added max and min function

--- code1.c
+++ code1.c
@@ -7,11 +7,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int max(double *ar);
+int min(double *ar);
+
 int main(){
 	FILE *salesReport;
 	FILE *fReport;
 	double sales[12];
 	char ch[50];
+	void monthlyReport(FILE fReport, double sales);
 	salesReport = fopen("test.txt", "r");
 	printf("Enter sales report file: \n");
 	for(int i = 0; i < 12; i++){
@@ -21,5 +25,28 @@ int main(){
 	fclose(salesReport);
 	fReport = fopen("Sales_Report.txt", "w");
 	fclose(fReport);
-	return 0;
-} 
+}
+
+int max(double *ar){
+	double max = 0.00;
+	int month;
+	for(int i = 0; i < 12; i++){
+		if(ar[i] > max){
+			max = ar[i];
+			month = i;
+		}
+	}
+	return month;
+}
+
+int min(double *ar){
+	double min = ar[max(ar)];
+	int month;
+	for(int i = 0; i<12; i++){
+		if(ar[i] < min){
+			min = ar[i];
+			month = i;
+		}
+	}
+	return month;
+}
